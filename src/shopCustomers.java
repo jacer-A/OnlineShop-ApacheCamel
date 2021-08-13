acdfa3f265aa186488153906df3dd15adefb48e6
jacer <aloui@campus.tu-berlin.de>
2021-08-14 00:21:31 +0200

    ..

--- src/shopCustomers.java
+++ src/shopCustomers.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class shopCustomers {
     public static HashMap<Integer, shopCustomers.Customer> Customers= new HashMap<>();
-
+    // test
 
 
     public static void main(String[] args) {
